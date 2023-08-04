6100033d0d73ec89fdfe74e94f321de87021bc54
DMakcRus <el16dmcr@leeds.ac.uk>
2023-08-04 16:08:31 +0100

    Deleted physics helper file

--- include/MuJoCoHelper.h
+++ include/MuJoCoHelper.h
@@ -5,84 +5,100 @@
 #ifndef PHYSICSSIMSWITCHING_MUJOCOHELPER_H
 #define PHYSICSSIMSWITCHING_MUJOCOHELPER_H
 
-#include "physicsSimulator.h"
+#include "stdInclude.h"
 #include "mujoco.h"
 #include <GLFW/glfw3.h>
 
-class MuJoCoHelper : public physicsSimulator {
+#define MAIN_DATA_STATE     -1
+#define MASTER_RESET_DATA   -2
+
+struct pose_7{
+    m_point position;
+    m_quat quat;
+};
+
+struct pose_6{
+    m_point position;
+    m_point orientation;
+};
+
+class MuJoCoHelper{
 public:
     // Constructor
     MuJoCoHelper(vector<robot> robots, vector<string> _bodies);
 
     // Utility functions -- robots
-    bool isValidRobotName(string robotName, int &robotIndex, string &robotBaseJointName) override;
-    bool setRobotJointsPositions(string robotName, vector<double> jointPositions, int dataIndex) override;
-    bool setRobotJointsVelocities(string robotName, vector<double> jointVelocities, int dataIndex) override;
-    bool setRobotJointsControls(string robotName, vector<double> jointControls, int dataIndex) override;
+    bool isValidRobotName(string robotName, int &robotIndex, string &robotBaseJointName);
+    bool setRobotJointsPositions(string robotName, vector<double> jointPositions, int dataIndex);
+    bool setRobotJointsVelocities(string robotName, vector<double> jointVelocities, int dataIndex);
+    bool setRobotJointsControls(string robotName, vector<double> jointControls, int dataIndex);
 
-    bool getRobotJointsPositions(string robotName, vector<double> &jointPositions, int dataIndex) override;
-    bool getRobotJointsVelocities(string robotName, vector<double> &jointVelocities, int dataIndex) override;
-    bool getRobotJointsAccelerations(string robotName, vector<double> &jointsAccelerations, int dataIndex) override;
-    bool getRobotJointsControls(string robotName, vector<double> &jointsControls, int dataIndex) override;
-    bool getRobotJointsGravityCompensaionControls(string robotName, vector<double> &jointsControls, int dataIndex) override;
+    bool getRobotJointsPositions(string robotName, vector<double> &jointPositions, int dataIndex);
+    bool getRobotJointsVelocities(string robotName, vector<double> &jointVelocities, int dataIndex);
+    bool getRobotJointsAccelerations(string robotName, vector<double> &jointsAccelerations, int dataIndex);
+    bool getRobotJointsControls(string robotName, vector<double> &jointsControls, int dataIndex);
+    bool getRobotJointsGravityCompensaionControls(string robotName, vector<double> &jointsControls, int dataIndex);
 
     // Utility functions -- bodies
-    bool isValidBodyName(string bodyName, int &bodyIndex) override;
-    bool setBodyPose_quat(string bodyName, pose_7 pose, int dataIndex) override;
-    bool setBodyPose_angle(string bodyName, pose_6 pose, int dataIndex) override;
-    bool setBodyVelocity(string bodyName, pose_6 velocity, int dataIndex) override;
+    bool isValidBodyName(string bodyName, int &bodyIndex);
+    bool setBodyPose_quat(string bodyName, pose_7 pose, int dataIndex);
+    bool setBodyPose_angle(string bodyName, pose_6 pose, int dataIndex);
+    bool setBodyVelocity(string bodyName, pose_6 velocity, int dataIndex);
 
-    bool getBodyPose_quat(string bodyName, pose_7 &pose, int dataIndex) override;
-    bool getBodyPose_angle(string bodyName, pose_6 &pose, int dataIndex) override;
-    bool getBodyVelocity(string bodyName, pose_6 &velocity, int dataIndex) override;
-    bool getBodyAcceleration(string bodyName, pose_6 &acceleration, int dataIndex) override;
+    bool getBodyPose_quat(string bodyName, pose_7 &pose, int dataIndex);
+    bool getBodyPose_angle(string bodyName, pose_6 &pose, int dataIndex);
+    bool getBodyVelocity(string bodyName, pose_6 &velocity, int dataIndex);
+    bool getBodyAcceleration(string bodyName, pose_6 &acceleration, int dataIndex);
 
     // Extras
-    Eigen::MatrixXd calculateJacobian(std::string bodyName, int dataIndex) override;
-    int checkSystemForCollisions(int dataIndex) override;
-    bool checkBodyForCollisions(string bodyName, int dataIndex) override;
+    Eigen::MatrixXd calculateJacobian(std::string bodyName, int dataIndex);
+    int checkSystemForCollisions(int dataIndex);
+    bool checkBodyForCollisions(string bodyName, int dataIndex);
 
     // ----- Loading and saving system states -----
-    bool appendSystemStateToEnd(int dataIndex) override;
-    bool checkIfDataIndexExists(int dataIndex) override;
-    bool copySystemState(int dataDestinationIndex, int dataSourceIndex) override;
-    bool deleteSystemStateFromIndex(int listIndex) override;
-    bool clearSystemStateList() override;
+    bool appendSystemStateToEnd(int dataIndex);
+    bool checkIfDataIndexExists(int dataIndex);
+    bool copySystemState(int dataDestinationIndex, int dataSourceIndex);
+    bool deleteSystemStateFromIndex(int listIndex);
+    bool clearSystemStateList();
 
     void cpMjData(const std::shared_ptr<mjModel> m, std::shared_ptr<mjData> d_dest, const std::shared_ptr<mjData> d_src);
     std::shared_ptr<mjData> returnDesiredDataState(int dataIndex);
 
     // ------------------------------- Visualisation -----------------------------------------
-    void initVisualisation() override;
-    void updateScene(GLFWwindow *window, const char* label) override;
-    void mouseMove(double dx, double dy, bool button_left, bool button_right,  GLFWwindow *window) override;
-    void scroll(double yoffset) override;
+    void initVisualisation();
+    void updateScene(GLFWwindow *window, const char* label);
+    void mouseMove(double dx, double dy, bool button_left, bool button_right,  GLFWwindow *window);
+    void scroll(double yoffset);
     mjvCamera cam;                   // abstract camera
     mjvScene scn;                    // abstract scene
     mjvOption opt;			        // visualization options
     mjrContext con;				    // custom GPU context
 
-    void initSimulator(double timestep, const char* fileName) override;
-    bool stepSimulator(int steps, int dataIndex) override;
-    bool forwardSimulator(int dataIndex) override;
-    bool forwardSimulatorWithSkip(int dataIndex, int skipStage, int skipSensor) override;
+    void initSimulator(double timestep, const char* fileName);
+    bool stepSimulator(int steps, int dataIndex);
+    bool forwardSimulator(int dataIndex);
+    bool forwardSimulatorWithSkip(int dataIndex, int skipStage, int skipSensor);
 
     void setupMuJoCoWorld(double timestep, const char* fileName);
 
     bool setBodyPosition(string bodyName, m_point position);
 
-    void initModelForFiniteDifferencing() override;
-    void resetModelAfterFiniteDifferencing() override;
+    void initModelForFiniteDifferencing();
+    void resetModelAfterFiniteDifferencing();
 
-    double returnModelTimeStep() override;
+    double returnModelTimeStep();
 
-    double* sensorState(int dataIndex, std::string sensorName) override;
+    double* sensorState(int dataIndex, std::string sensorName);
 
     vector<std::shared_ptr<mjData>> savedSystemStatesList;      // List of saved system states
     std::shared_ptr<mjData> d_master_reset;                     // Master reset mujoco data
     std::shared_ptr<mjData> mdata;                              // main MuJoCo data
     std::shared_ptr<mjModel> model;                             // MuJoCo model
     std::vector<std::shared_ptr<mjData>> fd_data;               // Finite differencing MuJoCo data - instantiated with the number of cores on the pc
+
+    vector<robot> robots;
+    vector<string> bodies;
 
 private:
     int save_iterations;
